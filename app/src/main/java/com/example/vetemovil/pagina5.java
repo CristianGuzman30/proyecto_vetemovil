5620d3de60793667a2dbb5741dc5e0d036194ed6
CarlosDiazPreciado <carlos.diazp@uniagustiniana.edu.co>
2023-05-24 10:36:50 -0500

    Se realizan varias paginas al proyecto

---
+++ app/src/main/java/com/example/vetemovil/pagina5.java
@@ -0,0 +1,31 @@
+package com.example.vetemovil;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class pagina5 extends AppCompatActivity{
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
